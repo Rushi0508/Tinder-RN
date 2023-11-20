a3d7fe235713d7455c48d9916694618f9838e030
Rushi_0508 <rushigandhi14@gmail.com>
2023-11-20 15:31:52 +0530

    feat: built login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,23 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../StackNavigator'
 import useAuth from '../hooks/useAuth'
-
+import tw from 'twrnc'
+import { SafeAreaView } from 'react-native-safe-area-context'
 type LoginProps = NativeStackScreenProps<RootStackParamList, 'Login'>
 
 const LoginScreen = ({navigation}: LoginProps) => {
 
   const {signInWithGoogle, GoogleSigninButton, isLoading}:any = useAuth();
 
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+
   return (
-    <View>
-      <Text> {isLoading? "Loading..": "LoginScreen"}</Text>
-      <GoogleSigninButton
-      
-        onPress={signInWithGoogle}
-      />
-    </View>
+    <SafeAreaView style={tw`flex-1`}>
+      <ImageBackground
+        source={{uri: "https://tinder.com/static/tinder.png"}}
+        resizeMode='cover'
+        style={tw`flex-1`}
+      >
+       <TouchableOpacity onPress={signInWithGoogle}  style={tw`absolute p-4 rounded-2xl bg-white bottom-40 w-52 mx-[25%]`}>
+        <Text style={tw`font-semibold text-center`}>SignIn & Get Swiping</Text> 
+       </TouchableOpacity>
+      </ImageBackground>
+    </SafeAreaView>
   )
 }
 
