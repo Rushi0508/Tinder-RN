6e02fdc1f14d2239e523d0e15ad4c8ec2f6317de
Rushi_0508 <rushigandhi14@gmail.com>
2023-11-21 13:19:11 +0530

    feat: header for chat screen

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -2,14 +2,17 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../StackNavigator'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../components/Header'
 
 type ChatProps = NativeStackScreenProps<RootStackParamList, 'Chat'>
 
 const ChatScreen = ({navigation}: ChatProps) => {
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Header title="Chat" callEnabled={false}/>
+
+    </SafeAreaView>
   )
 }
 
