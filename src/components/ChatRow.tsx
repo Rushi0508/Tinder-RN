c38842aab28225c34e52beb0cfd9e0bb2568b631
Rushi_0508 <rushigandhi14@gmail.com>
2024-04-21 23:23:43 +0530

    Fixed Key

--- src/components/ChatRow.tsx
+++ src/components/ChatRow.tsx
@@ -7,49 +7,49 @@ import tw from 'twrnc'
 import { db } from '../firebase';
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 
-const ChatRow = ({matchDetails}) => {
+const ChatRow = ({ matchDetails }) => {
 
     const navigation = useNavigation();
-    const {user}:any = useAuth();
+    const { user }: any = useAuth();
     const [matchedUserInfo, setMatchedUserInfo] = useState(null)
     const [lastMessage, setLastMessage] = useState(null)
-    useEffect(()=>{
+    useEffect(() => {
         setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, user.uid))
     }, [matchDetails, user])
 
-    useEffect(()=>
+    useEffect(() =>
         onSnapshot(
             query(
                 collection(db, "matches", matchDetails.id, "messages"),
                 orderBy('timestamp', 'desc')
-            ), snapshot=>
-                setLastMessage(snapshot.docs[0]?.data()?.message)
-        ), 
-        [matchDetails,db]
+            ), snapshot =>
+            setLastMessage(snapshot.docs[0]?.data()?.message)
+        ),
+        [matchDetails, db]
     )
 
-  return (
-    <TouchableOpacity
-    onPress={()=>{
-            //@ts-ignore
-            navigation.navigate('Message', {
-                matchDetails
-            })
-        }}
-        style={[tw`flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg shadow-[#000] shadow-offset-0/1 shadow-radius-1.41 elevation-2 shadow-opacity-20`]}
-    >
-        <Image
-            style={tw`rounded-full h-16 w-16 mr-4`}
-            source={{uri: matchedUserInfo?.photoURL}}
-        />
-        <View>
-            <Text style={tw`text-lg text-gray-700 font-semibold`}>
-                {matchedUserInfo?.displayName}
-            </Text>
-            <Text>{lastMessage || "Say Hi !!"}</Text>
-        </View>
-    </TouchableOpacity>
-  )
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                //@ts-ignore
+                navigation.navigate('Message', {
+                    matchDetails
+                })
+            }}
+            style={[tw`flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg shadow-[#000] shadow-offset-0/1 shadow-radius-1.41 elevation-2 shadow-opacity-20`]}
+        >
+            <Image
+                style={tw`rounded-full h-16 w-16 mr-4`}
+                source={{ uri: matchedUserInfo?.photoURL }}
+            />
+            <View>
+                <Text style={tw`text-lg text-gray-700 font-semibold`}>
+                    {matchedUserInfo?.displayName}
+                </Text>
+                <Text>{lastMessage || "Say Hi !!"}</Text>
+            </View>
+        </TouchableOpacity>
+    )
 }
 
 export default ChatRow
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,27 +1,31 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth, initializeAuth, getReactNativePersistence} from 'firebase/auth'
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-import {getFirestore} from 'firebase/firestore'
+import {initializeApp} from 'firebase/app';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB03dNx43VrGiDB8Svt1H6_j64UUgbAgi8",
-  authDomain: "tinder-5d836.firebaseapp.com",
-  projectId: "tinder-5d836",
-  storageBucket: "tinder-5d836.appspot.com",
-  messagingSenderId: "648695021147",
-  appId: "1:648695021147:web:1b67267b65e9c85326097e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
+  authDomain: 'tinder-5d836.firebaseapp.com',
+  projectId: 'tinder-5d836',
+  storageBucket: 'tinder-5d836.appspot.com',
+  messagingSenderId: '648695021147',
+  appId: '1:648695021147:web:1b67267b65e9c85326097e',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-})
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 const auth = getAuth();
 const db = getFirestore();
 
-export {auth,db}
+export {auth, db};
